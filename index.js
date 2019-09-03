f3e0dc491c7fd77395b753fa2b699bd3172db02b
jonathanboss17 <jtboss47@gmail.com>
2019-09-03 16:57:48 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 2; 
+let num2 = 31;
+const multiply = num1 * num2; 
+
+const random = (Math.random() > 0) ? Math.random() : false; 
+
+const mod = 24 % 5; 
+
+const max = Math.max(20, 5, 4, 9); 
